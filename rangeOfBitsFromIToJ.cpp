c3c661a7047a7000b643a47d8dc18650d63a54e0
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-07-30 23:14:45 +0530

    First Commit

---
+++ rangeOfBitsFromIToJ.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void range(int num,int i, int j){
+    int a=(~0) << (j+1);
+    int b=(1<<i)-1;
+
+    int mask=a|b;
+    num=num & mask;
+
+    cout<<num<<endl;
+}
+
+int main(){
+    range(15,1,3);
+    return 0;
+}
