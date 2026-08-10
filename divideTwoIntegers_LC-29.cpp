103ac0bb09b3b3679b8c59dc6416d8a945db8c18
Aashish Mulaniya <mulaniyaaashish@gmail.com>
2026-08-10 21:48:35 +0530

    Adding New Files

--- divideTwoIntegers_LC-29.cpp
+++ divideTwoIntegers_LC-29.cpp
@@ -4,20 +4,39 @@
 using namespace std;
 
 int divide(int dividend, int divisor) {
-        if(dividend==0)
-        return 0;
-        bool negative=(dividend<0) != (divisor<0);
+        // if(dividend==0)
+        // return 0;
+        // bool negative=(dividend<0) != (divisor<0);
+        // long long a=abs((long long)dividend);
+        // long long b=abs((long long)divisor);
+        // long long ans=0;
+        // while(a>=b){
+        //     a-=b;
+        //     ans++;
+        // }
+        // if(negative)
+        // ans=-ans;
+        // if(ans>INT_MAX)
+        // return INT_MAX;
+        // return ans;
+        if(dividend==INT_MIN && divisor==-1)
+        return INT_MAX;
+        bool negative=(dividend<0)!=(divisor<0);
         long long a=abs((long long)dividend);
         long long b=abs((long long)divisor);
         long long ans=0;
         while(a>=b){
-            a-=b;
-            ans++;
+            long long temp=b;
+            long long multiple=1;
+            while(a>=(temp<<1)){
+                temp<<=1;
+                multiple<<=1;
+            }
+            a-=temp;
+            ans+=multiple;
         }
         if(negative)
-        ans=-ans;
-        if(ans>INT_MAX)
-        return INT_MAX;
+            ans=-ans;
         return ans;
     }
 
